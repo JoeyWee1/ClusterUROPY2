20ace2ca6e5accd0e074091a58e783c7c35c6a3f
Joey <jyw22@ic.ac.uk>
2024-07-30 11:27:58 +0100

    Minor changes

--- Offset NN Cal/Z from Y Axis Gradient NN Cal.py
+++ Offset NN Cal/Z from Y Axis Gradient NN Cal.py
@@ -229,7 +229,7 @@ def train_test_model(sc, training_years, n_epochs, batch_size):
     plt.savefig("./Outputs_Z_From_Y_Axis/C{}/NN/Basic NN {} years {} epochs v2.png".format(sc+1, training_years, n_epochs))
 
 for i in range(0,4):
-    train_test_model(i, 15, 5000, 10)
-    train_test_model(i, 15, 5000, 25)
+    train_test_model(i, 15, 500, 10)
+    train_test_model(i, 15, 500, 25)
 
         
